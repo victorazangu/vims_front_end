75131129c9f831378d361a48ea18e8efe531ac21
victorazangu <victorsazangu@gmail.com>
2023-03-31 23:39:10 +0300

    adding blogs and admins designs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import "./App.css";
 import Navbar from "./components/header/Navbar";
-// import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/Footer";
 // import Pagination from "./components/pagination/Pagination";
 // import Login from "./components/Login/Login";
-import SignUp from "./components/signup/SignUp";
+// import SignUp from "./components/signup/SignUp";
+import AllBlogs from "./components/blogs/AllBlogs/AllBlogs";
+import BlogCard from "./components/blogs/single_blog/BlogCard";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <Pagination /> */}
-      {/* <Footer /> */}
+
       {/* <Login/> */}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <div className="row">
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+        <div className="col">
+          {" "}
+          <BlogCard />
+        </div>
+      </div>
+
+      {/* <AllBlogs/> */}
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/blogs/single_blog/BlogCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+
+function BlogCard() {
+  return (
+    <MDBCard>
+      <MDBCardImage
+        src="https://mdbootstrap.com/img/new/standard/nature/184.webp"
+        position="top"
+        alt="..."
+      />
+      <MDBCardBody>
+        <MDBCardTitle>Card title</MDBCardTitle>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </MDBCardText>
+        <MDBBtn href="#">Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+  );
+}
+
+export default BlogCard;
